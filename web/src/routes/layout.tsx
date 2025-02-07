60f7f792c534e709841024152bd8e5f774342e00
Robert MacWha <trebor.ahwcam@gmail.com>
2025-02-06 19:53:57 -0500

    feat: Load checklist from local yamlContent
    Added `@rollup/plugin-yaml`
    Moved checklist yaml to `web/src/assets`

--- web/src/routes/layout.tsx
+++ web/src/routes/layout.tsx
@@ -2,17 +2,14 @@ import { component$, useContextProvider, Slot } from "@builder.io/qwik";
 import { routeLoader$, type RequestHandler } from "@builder.io/qwik-city";
 import jsyaml from "js-yaml";
 
+import yamlContent from "~/assets/checklist.yml?raw";
 import Navbar from "~/components/furniture/nav";
 import Footer from "~/components/furniture/footer";
 import { ChecklistContext } from "~/store/checklist-context";
 import type { Sections } from "~/types/PSC";
 
 export const useChecklists = routeLoader$(async () => {
-  const remoteUrl = 'https://raw.githubusercontent.com/Lissy93/personal-security-checklist/HEAD/personal-security-checklist.yml';
-  return fetch(remoteUrl)
-    .then((res) => res.text())
-    .then((res) => jsyaml.load(res) as Sections)
-    .catch(() => []);
+  return jsyaml.load(yamlContent) as Sections;
 });
 
 export const onGet: RequestHandler = async ({ cacheControl }) => {
